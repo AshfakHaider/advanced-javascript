5e10d6504a88549f49ae46cd3ff9fc746fa9c7db
AshfakHaider <nawazishhaider31@gmail.com>
2021-04-09 01:01:17 +0600

    double == vs trile ===

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,19 @@
+const first = 5;
+const second ="5";
+if(first == second){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+
+
+const first = 5;
+const second ="5";
+if(first === second){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
