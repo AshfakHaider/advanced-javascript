2388e6b5674260f2ba0c8fe74276cac77d87ec67
AshfakHaider <nawazishhaider31@gmail.com>
2021-04-09 00:24:47 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+let value;
+console.log(value);
+
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+
+const result = add(5,10);
+console.log(result);
+
+
+function add(num1,num2){
+    console.log(num1 + num2);
+}
+
+const result = add(10);
+console.log(result);
+
+
+const variable = {name: "manik", phone: 12345}
+console.log(variable.address);
